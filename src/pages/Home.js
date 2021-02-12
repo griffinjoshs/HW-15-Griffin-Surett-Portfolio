a3de4e64c7fd8f864a2e65bd2b537c67caeb8208
griffinjoshs <leveloutusa@gmail.com>
2021-02-11 21:53:14 -0500

    new gh-pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import '../App.css'
-import HeroSection from '../components/HeroSection/HeroSection'
-import FourIconSec from '../components/FourIconSec/FourIconSec'
-import HomeCardSec from '../components/HomeCardSec/HomeCardSec'
-// import TestimonialCard from '../components/HomeTestSec/HomeTestSec' 
-import Footer from '../components/Footer/Footer'
-import hero from '../../src/components/HeroSection/hero.json'
-import Parallax from '../components/HomeParallax/HomeParallax'
-import ContactInfo from '../components/ContactInfo/ContactInfo'
-
+import React from "react";
+import "../App.css";
+import HeroSection from "../components/HeroSection/HeroSection";
+import FourIconSec from "../components/FourIconSec/FourIconSec";
+import HomeCardSec from "../components/HomeCardSec/HomeCardSec";
+// import TestimonialCard from '../components/HomeTestSec/HomeTestSec'
+import Footer from "../components/Footer/Footer";
+import hero from "../../src/components/HeroSection/hero.json";
+import Parallax from "../components/HomeParallax/HomeParallax";
+import ContactInfo from "../components/ContactInfo/ContactInfo";
 
 export const Home = () => {
-    return (
-        <>
-          <HeroSection 
-          Title={hero[0].heroTitle}
-          Paragraph={hero[0].heroSubPost}
-          PrimaryBtn={hero[0].heroPrimary}
-          OutlineBtn={hero[0].heroOutline}
-          />
-          <FourIconSec />
-          <HomeCardSec />
-          <Parallax/>
-          <ContactInfo/>
-        </>
-    )
-}
+  return (
+    <>
+      <HeroSection
+        Title={hero[0].heroTitle}
+        Paragraph={hero[0].heroSubPost}
+        PrimaryBtn={hero[0].heroPrimary}
+        OutlineBtn={hero[0].heroOutline}
+      />
+      <FourIconSec />
+      <HomeCardSec />
+      <Parallax />
+      <ContactInfo />
+    </>
+  );
+};
+
+export default Home;
