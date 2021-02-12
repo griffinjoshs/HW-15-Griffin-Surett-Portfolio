d3fd6f44388e0fe86e6d845f00ec0c7be3e2891b
griffinjoshs <leveloutusa@gmail.com>
2021-02-11 21:47:40 -0500

    gh-pages commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
-import { Contact } from './pages/Contact'
+import { Contact } from './pages/contact'
 import Footer from "./components/Footer/Footer";
 import { Portfolio } from './pages/Portfolio'
 
@@ -16,7 +16,7 @@ function App() {
     <Navbar />
     <Switch>
       <Route path='/' exact component={Home}/>
-      <Route path='/contact' exact component={Contact}/>
+      <Route exact path='/contact' component={Contact}/>
       <Route path='/portfolio' exact component={Portfolio}/>
     </Switch>
     <Footer/>
